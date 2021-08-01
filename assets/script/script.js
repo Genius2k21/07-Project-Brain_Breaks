508a0c5b9274944d545095856668e2d0d80d7e86
scampos321 <sebastian.campos321@gmail.com>
2021-08-01 16:33:46 -0400

    fixed script file

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,26 +1,3 @@
 // This handles the main section of the screens / hiding screens and displaying screens 
 
 // TO DO: make more script files for different features 
-const getName = document.querySelector('#entername');
-const submitBtn = document.querySelector('#submitname');
-const form = document.querySelector('form');
-
-const h2 = document.querySelector('h2');
-
-form.addEventListener('submit', function(event){
-    event.preventDefault();
-});
-
-submitBtn.addEventListener('click', function(){
-    localStorage.setItem('name', getName.value);
-    displayName();
-});
-
-function displayName(){
-    if(localStorage.getItem('name')){
-        const name = localStorage.getItem('name');
-        h2.textContent = `Welcome ${name}!`;
-    }
-}
-
-document.body.onload = displayName;
