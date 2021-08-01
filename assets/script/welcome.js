fd09f0e020478d29586dc28ac3987fe89dd0b3b5
scampos321 <sebastian.campos321@gmail.com>
2021-08-01 16:29:44 -0400

    added form feature

--- assets/script/welcome.js
+++ assets/script/welcome.js
@@ -1,6 +1,3 @@
-// This handles the main section of the screens / hiding screens and displaying screens 
-
-// TO DO: make more script files for different features 
 const getName = document.querySelector('#entername');
 const submitBtn = document.querySelector('#submitname');
 const form = document.querySelector('form');
