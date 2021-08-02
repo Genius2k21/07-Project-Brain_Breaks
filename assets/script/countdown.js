15ddac92e7ad5257974167ac31c7c4256cb2bec2
Hudson Hancock <hudsonhancock@gmail.com>
2021-08-02 15:54:02 -0400

    Added html for time input and clock screens. Stored time as seconds in global variable to be used to generate countdown

---
+++ assets/script/countdown.js
@@ -0,0 +1,29 @@
+var play = document.querySelector('#playBtn');
+var outline = document.querySelector('.track_outline circle');
+var movingOutline = document.querySelector('.moving_outline circle');
+var addMinute = document.querySelector('#plus_one');
+var timeDisplay = document.querySelector('#time_display');
+var setTimeBtn = document.querySelector("#set_time_btn")
+
+movingOutlineLength = outline.getTotalLength();
+console.log(movingOutlineLength);
+
+movingOutline.style.strokeDasharray = movingOutlineLength;
+movingOutline.style.strokeDashoffset = movingOutlineLength; 
+
+var focusTime = []
+
+setTimeBtn.addEventListener("click", function(){
+    focusTime = [];
+    var selectedTime = document.querySelector('#time_select').value;
+    seconds = selectedTime * 60;
+    var formattedTime = new Date(seconds * 1000).toISOString().substr(12, 4)
+
+    timeDisplay.textContent = formattedTime;
+});
+
+
+
+
+
+
