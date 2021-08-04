f388496bd1f5e206eae418f1290a33ac24d1a737
Amanda Morgan <AmandaC0022@gmail.com>
2021-08-04 09:26:46 -0500

    added comments on welcome js

--- assets/script/welcome.js
+++ assets/script/welcome.js
@@ -1,18 +1,27 @@
 const getName = document.querySelector('#entername');
 const submitBtn = document.querySelector('#submitname');
 const form = document.querySelector('form');
+const h2 = document.getElementById("welcome-alert"); 
 
-const h2 = document.querySelector('h2');
+//global variable for user name input 
+var userName; 
+
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
 });
 
+//when user clicks submit button, then... 
 submitBtn.addEventListener('click', function(){
-    localStorage.setItem('name', getName.value);
+    //grabs user's name from input 
+    userName = getName.value; 
+    //saves user's name into local storage under name 
+    localStorage.setItem('name', userName);
+    //runs displayName function 
     displayName();
 });
 
+//when user submits their name, it displays user's name underneath submit button 
 function displayName(){
     if(localStorage.getItem('name')){
         const name = localStorage.getItem('name');
