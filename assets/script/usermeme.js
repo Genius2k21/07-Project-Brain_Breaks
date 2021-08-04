f1878c49eaa1256a7b608abbe81e143fca75bc38
Amanda Morgan <AmandaC0022@gmail.com>
2021-08-04 09:52:24 -0500

    added function to get user's choice value

--- assets/script/usermeme.js
+++ assets/script/usermeme.js
@@ -13,11 +13,22 @@ function getMeme(){
           console.log(data[0].data.children[0].data.url_overridden_by_dest);
       })
 }
+function getUserChoice(event) {
+  //this logs the value of the user's choice button 
+  var userChoice = event.target.value;
+  console.log(userChoice); 
+}
 
 var catBtn = document.querySelector('#catBtn');
 
-catBtn.addEventListener('click', getMeme);
+catBtn.addEventListener('click', function(event) {
+  getUserChoice(event);
+  getMeme();  
+});
 
 var dogBtn = document.querySelector('#dogBtn');
 
-dogBtn.addEventListener('click', getMeme);
+dogBtn.addEventListener('click', function(event) {
+  getUserChoice(event); 
+  getMeme(); 
+});
